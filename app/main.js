5ea055f716c1d94dd92c8ddf108e30962c34bc75
jingwen@xiaoshijie.com <jingwen@xiaoshijie.com>
2016-03-18 16:08:07 +0800

    调整结构

--- app/main.js
+++ app/main.js
@@ -14,8 +14,10 @@ app.on('ready',function(){
 	mainWindow = new BrowserWindow({width: 800,height: 600});
 
 	mainWindow.loadURL('file://'+__dirname+'/index.html');
-
+	
 	mainWindow.on('closed',function(){
 		mainWindow = null;
 	});
+
+	mainWindow.setTitle("我的笔记markdown");
 });
