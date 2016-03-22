a34c9ba7148c2083c0d340b70636d82792a11a27
jingwen@xiaoshijie.com <jingwen@xiaoshijie.com>
2016-03-22 18:23:48 +0800

    调整结构

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ app.on('window-all-closed',function(){
 app.on('ready',function(){
 	mainWindow = new BrowserWindow({width: 800,height: 600});
 
-	mainWindow.loadURL('file://'+__dirname+'/index.html');
+	mainWindow.loadURL('file://'+__dirname+'/app/index.html');
 	
 	mainWindow.on('closed',function(){
 		mainWindow = null;
