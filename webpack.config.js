e2de0f61c44a4f44adf9aa1a50e82f968611a4bf
jingwen@xiaoshijie.com <jingwen@xiaoshijie.com>
2016-03-24 15:46:51 +0800

    添加less 修改库文件剥离配置 完成loading设计稿

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,28 @@
 var webpack = require('webpack');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var HtmlPlugin = require('html-webpack-plugin');
+var less = require("less");
+
+var LessPluginCleanCSS = require('less-plugin-clean-css');
+
 
 module.exports = {
 	entry:{
-		'app':'./src/public/js/app.js'
+		'app':'./src/public/js/app.js',
+		'Vue' : ['vue']
 	},
 	output:{
 		path: 'app/public/js',
 		filename: '[name].js',
-		publicPath:'./public/js'
+		publicPath:'./public/js',
 	},
 	devtool: false,
 	module:{
 		'loaders':[
+			{
+            	test:/\.less$/,
+				loader: ExtractTextPlugin.extract("style-loader", "css-loader!less-loader") 
+            },
 			{
 				test : /\.css$/,
 				loader : ExtractTextPlugin.extract('style', 'css')
@@ -21,30 +30,35 @@ module.exports = {
 			{
                 test : /\.js$/,
                 loader : 'babel-loader',
-                exclude: /node_modules/   
+                exclude: /node_modules/
             }
 		]
 	},
+	lessLoader: {
+	    lessPlugins: [
+	      new LessPluginCleanCSS({advanced: true})
+	    ]
+	},
 	plugins:[
 		new ExtractTextPlugin("../css/[name].css",{
-			allChunks:true
+			// allChunks:true
 		}),
 		new HtmlPlugin({
 			template: './src/index.html',
 			filename: '../../index.html',
 			inject: 'body',
 			hash: true,
-			chunks: ['app','Vue','Wilddog'],
+			chunks: ['app','Vue'],
 			minify: {   
 				removeComments: true,
 				collapseWhitespace: false
 			}
 		}),
 		new webpack.ProvidePlugin({
-            Vue:'vue',
-            Wilddog:'wilddog'
+            Vue:'vue'
+            // Wilddog:'wilddog'
         }),
-        new webpack.optimize.CommonsChunkPlugin('Vue','../lib/Vue.js'),
-        new webpack.optimize.CommonsChunkPlugin('Wilddog','../lib/Wilddog.js'),
+        new webpack.optimize.CommonsChunkPlugin('Vue','../lib/Vue.js')
+        // new webpack.optimize.CommonsChunkPlugin('Wilddog','../lib/Wilddog.js'),
 	]
 }
