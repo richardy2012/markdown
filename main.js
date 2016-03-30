fa1c91595eca7ed72b2e2e610a5602229ea7d5f1
Zjingwen <309940379@qq.com>
2016-03-30 16:52:21 +0800

    添加快捷键 实现功能： 复制、粘贴、全选、剪除 问题： 全局快捷键，会屏蔽系统自带的快捷键

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 /*----------------------- electron配置 --------------------------*/
 var app = require('app');
 var BrowserWindow = require('browser-window');
-
+var globalShortcut = require('global-shortcut');
 
 var mainWindow = null;
 
@@ -10,17 +10,46 @@ app.on('window-all-closed',function(){
 	if(process.plathfrom != 'darwin'){
 		app.quit();
 	}
+	globalShortcut.unregister('option+command+i');
+	globalShortcut.unregister('command+c');
+	globalShortcut.unregister('command+a');
+	globalShortcut.unregister('command+v');
+	globalShortcut.unregister('command+x');
+	globalShortcut.unregisterAll();
 });
 
 app.on('ready',function(){
 	mainWindow = new BrowserWindow({width: 800,height: 600});
 
 	mainWindow.loadURL('file://'+__dirname+'/app/index.html');
-	
+
 	mainWindow.on('closed',function(){
 		mainWindow = null;
 	});
 
+	globalShortcut.register('option+command+i',function(){
+		mainWindow.toggleDevTools();
+	});
+
+	globalShortcut.register('command+c',function(){
+		console.log("复制");
+		mainWindow.copy();
+	});
+
+	globalShortcut.register('command+a',function(){
+		console.log("全选");
+		mainWindow.selectAll();
+	});
+
+	globalShortcut.register('command+v',function(){
+		console.log("粘贴")
+		mainWindow.paste();
+	});
+
+	globalShortcut.register('command+x',function(){
+		console.log("剪除");
+		mainWindow.cut();
+	});
 	mainWindow.setTitle("我的markdown");
 });
 /*----------------------- electron配置 end-----------------------*/
